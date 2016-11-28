e38bb7befec85d67aec1c289e35d819687f9fd41
Tedozi Manson <duyphan.developer@gmail.com>
2016-11-28 10:19:16 +0700

    Refactor

---
+++ src/Providers/ConsoleServiceProvider.php
@@ -0,0 +1,47 @@
+<?php namespace WebEd\Base\ThemesManagement\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class ConsoleServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->generatorCommands();
+        $this->otherCommands();
+    }
+
+    private function generatorCommands()
+    {
+        $this->commands([
+            \WebEd\Base\ThemesManagement\Console\Generators\MakeTheme::class,
+            \WebEd\Base\ThemesManagement\Console\Generators\MakeController::class,
+            \WebEd\Base\ThemesManagement\Console\Generators\MakeView::class,
+            \WebEd\Base\ThemesManagement\Console\Generators\MakeCommand::class,
+        ]);
+    }
+
+    private function otherCommands()
+    {
+        $this->commands([
+            \WebEd\Base\ThemesManagement\Console\Commands\EnableThemeCommand::class,
+            \WebEd\Base\ThemesManagement\Console\Commands\DisableThemeCommand::class,
+            \WebEd\Base\ThemesManagement\Console\Commands\InstallThemeCommand::class,
+            \WebEd\Base\ThemesManagement\Console\Commands\UninstallThemeCommand::class,
+        ]);
+    }
+}
